693ad62645d6749c2b49574a061a3402640151f8
opaluwa john <johnopaluwa@gmail.com>
2017-12-08 17:19:34 +0100

    add missing contact-us-service

---
+++ src/app/contact-us.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router'
+
+interface redirectURL{
+  sent: string;
+}
+
+@Injectable()
+export class ContactUsService {
+  constructor(private  _http: HttpClient, private router: Router) { }
+
+  sendMail(message): any {
+    return this._http.post<redirectURL>('/api/contact', message);
+  }
+}
