1fc3a375324b902fe3b0d1e24beb8831a944228d
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:22:00 +0100

    update import URL

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ContactUsService} from '../contact-us.service';
+import {ContactUsService} from '../_services/contact-us.service';
 
 @Component({
   selector: 'app-contact',
