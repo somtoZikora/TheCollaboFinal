48ea65b2bbc77f211b831963991a905b2aa8c695
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:16:09 +0100

    add bcrypt and someh helper fuctions for user schema

--- models/User.js
+++ models/User.js
@@ -3,28 +3,56 @@
  */
 
 var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
+var bcrypt = require('bcryptjs');
+var UserSchema = mongoose.Schema({
   email:{
     type: String,
     unique: true,
-   // required: true,
+    required: true,
     trim: true
   },
   username:{
     type: String,
     unique: true,
-   // required: true,
+    required: true,
     trim: true
   },
   password:{
     type: String,
-   // required: true
+    required: true
   },
+  token: String,
   firstname: String,
   lastname: String,
   language: String,
   courseofstudy: String,
   degree: String
 });
-var  User =mongoose.model('User', UserSchema);
-module.exports = User
+const  User = module.exports =mongoose.model('User', UserSchema);
+//module.exports = User
+
+module.exports.addUser = (newUser, callback) => {
+bcrypt.genSalt(10, (err, salt) =>{
+  bcrypt.hash(newUser.password, salt, (err, hash) => {
+    if(err) throw  err;
+    newUser.password = hash;
+    newUser.save(callback);
+  });
+});
+}
+
+module.exports.getUserById = (id, callback) =>{
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.comparePassword = (candidatePassword, hash, callback) =>{
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) =>{
+    if(err) throw err;
+    callback(null, isMatch);
+  })
+}
