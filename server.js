e58a1c4a96b954c87476452999a5c5de9877eb2c
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:43:13 +0100

    added more nodemodules added more routes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 var http = require('http');
 const mongoose = require('mongoose')
 const expressValidator = require('express-validator');
+const passport = require('passport');
+
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
+var passportCofig = require('./config/passport');
+var cors = require('cors');
 
 /**
  * load enviroment variables into process
@@ -20,7 +26,6 @@ const contactController = require('./controllers/contact');
 const userController = require('./controllers/user')
 
 var port = 3000;
-
 var app = express();
 
 /*connect to mongo db*/
@@ -31,10 +36,6 @@ mongoose.connection.on('error', (err)=>{
   console.log('%s MongoDB connection error. Please make sure MongoDB is running')
 })
 
-
-//load enviroment variables from .env file where API Keys and passwords are configured
-//dotenv.load({path: '.env.example'});
-
 //View Engine
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
@@ -43,25 +44,36 @@ mongoose.connection.on('error', (err)=>{
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Body Parser MW
+// Middle ware to Allow API request from different domains
+app.use(cors());
+
+// Middle ware Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 
-//app.use('/', index);
+//Middle ware passport
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/api/contact', contactController.getContact);
-app.post('/api/contact', contactController.postContact);
-app.post('/api/signup', userController.postSignup);
-app.post('/api/update-info', userController.postUpdateInfo)
+passportCofig.strategy(passport);
+
+app.post('/user/signup', userController.postSignup);
+app.post('/user/authenticate', userController.postAuthenticate);
+app.get('/user/profile',passport.authenticate('jwt', {session: false}), userController.getMe);
+
+app.get('/user/contact', contactController.getContact);
+app.post('/user/contact', contactController.postContact);
+
+
+app.post('/user/update-info', userController.postUpdateInfo);
+app.post('/user/signin', userController.postSignIn);
+
 
 /* Send all other requests to angular app */
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, 'dist/index.html' ))
 })
-
-
-//app.use('/api', tasks);
 
 app.listen(port, function(){
   console.log('Server started on port '+port);
