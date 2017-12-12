9286b4285ad2457f21550ef241cd8aa3422d3a89
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:12:03 +0100

    added route authentication middleware

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+/**
+ * Created by opaluwa john on 12/10/2017.
+ */
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const User = require('../models/User');
+
+exports.strategy = (passport) => {
+  console.log('i am here ere hree')
+  var opts = {};
+  opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = process.env.JWT_SECRET
+  opts.authScheme= 'bearer'
+  passport.use(new JwtStrategy(opts,function (jwt_payload, done) {
+    console.log(jwt_payload)
+    User.getUserById(jwt_payload.user._id, (err, user) => {
+      if(err){
+        return done(err, false);
+      }
+      if(user){
+        return done(null, user);
+      }else {
+        return done(null, false);
+      }
+    });
+  }))
+}
+
+exports.ensureAuthorized = (req, res, next) => {
+  var bearerToken;
+  var bearerHeader = req.headers["authorization"];
+  if(typeof  bearerHeader !== 'undefined'){
+    var bearer = bearerHeader.split(" ");
+    bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  }else {
+    res.send(403)
+  }
+}
