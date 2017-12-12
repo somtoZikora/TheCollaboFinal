ec6c3a7435cc07e8a02ee5931dace808553011e1
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:28:49 +0100

    add validate service for checks on user input

---
+++ src/app/_services/validate.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidateService {
+
+  constructor() { }
+
+  validateSignup(user): boolean {
+    if (user.username === undefined || user.email === undefined || user.password === undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  validateEmail(email): boolean {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+}
