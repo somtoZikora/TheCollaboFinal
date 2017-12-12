72b7634bf3c03344b9cd5a35f3d1aec880d1b4b2
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:21:11 +0100

    change post route and change file location

--- src/app/_services/contact-us.service.ts
+++ src/app/_services/contact-us.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router'
+import {Router} from '@angular/router';
 
-interface redirectURL{
+interface redirectURL {
   sent: string;
 }
 
@@ -12,6 +12,6 @@ export class ContactUsService {
   constructor(private  _http: HttpClient, private router: Router) { }
 
   sendMail(message): any {
-    return this._http.post<redirectURL>('/api/contact', message);
+    return this._http.post<redirectURL>('/user/contact', message);
   }
 }
