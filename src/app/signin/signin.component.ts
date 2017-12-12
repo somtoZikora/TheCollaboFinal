af85673575961cfe42fe4e0df27eeaa9396620d8
opaluwa john <johnopaluwa@gmail.com>
2017-12-12 12:31:50 +0100

    add signin component

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../_services/authentication.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  username: string;
+  password: string;
+  constructor(private _AuthenticationService: AuthenticationService,
+              private _Router: Router,
+              private _FlashMessageService: FlashMessagesService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    const User = {
+      username: this.username,
+      password: this.password
+    }
+    this._AuthenticationService.loginAuthenticateUser(User).subscribe(data => {
+      if (data.success) {
+        this._AuthenticationService.storeUserData(data.token, data.user);
+        this._FlashMessageService.show('You are now logged In', {cssClass: 'alert-success', timeout: 5000});
+        this._Router.navigate(['/dashboard']);
+      } else {
+        this._FlashMessageService.show(data.message, {cssClass: 'alert-danger', timeout: 5000});
+        this._Router.navigate(['/login']);
+      }
+    });
+  }
+
+}
