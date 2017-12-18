b90c524fb723f3475d5eeb5eb7308051388893aa
opaluwa john <johnopaluwa@gmail.com>
2017-12-18 23:36:17 +0100

    added imports

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -4,19 +4,29 @@
 import {NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import {
   FooterComponent,
   NavbarComponent,
+  SigninComponent,
+  SignupComponent,
 } from './components';
 
 const BASE_MODULES = [
   CommonModule,
-  RouterModule];
+  RouterModule,
+  FormsModule,
+  HttpClientModule,
+ ];
 
 const COMPONENTS = [
   FooterComponent,
-  NavbarComponent
+  NavbarComponent,
+  SigninComponent,
+  SignupComponent
 ];
 
 @NgModule({
