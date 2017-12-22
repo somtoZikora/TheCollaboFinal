613d001636e92ab22081b5e543d34c62e0addbf1
opaluwa john <johnopaluwa@gmail.com>
2017-12-22 05:51:08 +0100

    create profile component and its dependencies

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from '../../@core/services/authentication.service';
+import { Component } from '@angular/core';
+import {AuthenticationService} from '../../@core/services/authentication/authentication.service';
 import {Router} from '@angular/router';
+import {ProfileService} from '../../@core/services/profile/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-
-  user: object;
+export class ProfileComponent  {
   constructor(private _AuthenticationService: AuthenticationService,
-              private _Router: Router) { }
-
-  ngOnInit() {
-    this._AuthenticationService.getProfile().subscribe(profile => {
-      console.log(profile.user)
-      this.user = profile.user;
-    },
-    err => {
-      console.log(err);
-      return false;
-    });
+              private _Router: Router,
+              private _ProfileService: ProfileService) {}
+  logOut() {
+    this._AuthenticationService.logout();
+    this._Router.navigate(['/login']);
   }
-
+  onCancelClick() {
+    this._ProfileService.changeMessage('hideProfileComponentFromNavbar');
+  }
 }
+
