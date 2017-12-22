04729296f19d6a6f0fb092e0a7390ae86b1619d0
opaluwa john <johnopaluwa@gmail.com>
2017-12-22 05:47:45 +0100

    added show-messge-component and its dependencies

---
+++ src/app/pages/study-groups/showSelect/showSelect.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-select',
+  templateUrl: './showSelect.component.html',
+  styleUrls: ['./showSelect.component.css']
+})
+export class ShowSelectComponent {
+}
