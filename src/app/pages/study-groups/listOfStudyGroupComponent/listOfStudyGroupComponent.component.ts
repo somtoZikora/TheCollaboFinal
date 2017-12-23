416875c3ef8a4f9e282dc1946bdfc99e329a8e17
opaluwa john <johnopaluwa@gmail.com>
2017-12-23 04:15:31 +0100

    removed loading data fuctionality with service

--- src/app/pages/study-groups/listOfStudyGroupComponent/listOfStudyGroupComponent.component.ts
+++ src/app/pages/study-groups/listOfStudyGroupComponent/listOfStudyGroupComponent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter   } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter , Input   } from '@angular/core';
 import {StudyGroupService} from '../../../@core/services/study-group/study-group.service';
 
 @Component({
@@ -6,22 +6,16 @@ import {StudyGroupService} from '../../../@core/services/study-group/study-group
   templateUrl: './listOfStudyGroupComponent.component.html',
   styleUrls: ['./listOfStudyGroupComponent.component.css']
 })
-export class ListOfStudyGroupComponent implements OnInit {
+export class ListOfStudyGroupComponent {
 
-  listOfStudyGroups: Array<any> = []
   @Output() messageEvent = new EventEmitter<string>();
+  @Input() listOfStudyGroups: any;
+  @Input() listOfStudyGroupError: any;
 
   constructor(private _StudyGroupService: StudyGroupService) {}
 
-  ngOnInit() {
-  this._StudyGroupService.getListOfStudyGroups().subscribe(data => {
-    this.listOfStudyGroups = data;
-    console.log(this.listOfStudyGroups);
-  });
-  }
-
   onCancelClick(choice) {
-    if(choice === 'cancel') {
+    if (choice === 'cancel') {
       this.messageEvent.emit('hideListOfStudyGroupComponentFromListOfStudyGroupComponent');
     }
   }
