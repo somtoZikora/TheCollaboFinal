77e62925b26d0b66c31b34907edba7fd5dc2b01d
opaluwa john <johnopaluwa@gmail.com>
2017-12-29 20:00:05 +0100

    Refactor schema

--- models/Studygroup.js
+++ models/Studygroup.js
@@ -4,9 +4,26 @@
 /**
  * Created by opaluwa john on 12/8/2017.
  */
+/*var mongoose = require('mongoose');
+var StudyGroupSchema = mongoose.Schema({
+  groupName: String,
+  groupMembers: []
+});
+const  StudyGroup = module.exports =mongoose.model('StudyGroup', StudyGroupSchema);
+*/
+
+/**
+ * Created by opaluwa john on 12/21/2017.
+ */
+/**
+ * Created by opaluwa john on 12/8/2017.
+ */
 var mongoose = require('mongoose');
 var StudyGroupSchema = mongoose.Schema({
-  name: String
+  groupName: { type : String , unique : true },
+  groupMembers: [{ type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'}],
+  messages: [{sender: String, message: String}]
 });
 const  StudyGroup = module.exports =mongoose.model('StudyGroup', StudyGroupSchema);
 
